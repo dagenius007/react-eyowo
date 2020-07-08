dc9a41b5a1a01a23acf236aa03edd1aa5d3fbb80
Joshua Oluikpe <joshua.o@softcom.ng>
2020-07-08 20:32:14 +0100

    Payment verification status

--- sample/src/App.js
+++ sample/src/App.js
@@ -3,7 +3,7 @@ import useEyowoPayment from './lib/EyowoHooks';
 
 const App = () => {
 	const [loading, makePayment, verificationStatus] = useEyowoPayment({
-		billId: '5f0600e0d86bab39ec17ecf9',
+		billId: '5f061df6d86bab39ec17ecfd',
 		email: 'joshuaoluikpe@gmail.com',
 		eyowoToken: 'DTGFJ5N-QS84G18-NKWV9PJ-65T9Y9S',
 		_callback: function () {
